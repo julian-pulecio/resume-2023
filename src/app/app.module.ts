feb103fbe6c7982d5e7e0274ad212a8580b1473a
julian pulecio <developer@julianpulecio.com>
2023-07-13 19:11:12 -0500

    the cognito user pool client section is added to the cognito-auth-jwt-roles article

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { HomeComponent } from './components/home/home.component';
+
 
 
 @NgModule({
@@ -25,9 +27,24 @@ import { HomeComponent } from './components/home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxMasonryModule
+    NgxMasonryModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        coreLibraryLoader: () => import('highlight.js/lib/core'),
+        lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'), // Optional, only if you want the line numbers
+        languages: {
+          typescript: () => import('highlight.js/lib/languages/typescript'),
+          css: () => import('highlight.js/lib/languages/css'),
+          xml: () => import('highlight.js/lib/languages/xml'),
+          yaml: () => import('highlight.js/lib/languages/yaml')
+
+        },
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/articles/cognito-auth-jwt-roles/cognito-auth-jwt-roles.component.ts
+++ src/app/components/articles/cognito-auth-jwt-roles/cognito-auth-jwt-roles.component.ts
@@ -1,13 +1,108 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  HighlightAutoResult,
+  HighlightLoader,
+  HighlightModule,
+  HighlightOptions,
+  HIGHLIGHT_OPTIONS,
+} from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-cognito-auth-jwt-roles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HighlightModule],
   templateUrl: './cognito-auth-jwt-roles.component.html',
   styleUrls: ['./cognito-auth-jwt-roles.component.css']
 })
 export class CognitoAuthJwtRolesComponent {
+  
+  response!: HighlightAutoResult;
 
+  code = ` npm install -g serverless`;
+  code1 = ` # Create a new serverless project
+  serverless`
+  code2 = ` aws-cognito-auth-with-jwt-roles
+  ├─ .gitignore
+  ├─ handler.py
+  ├─ README.md
+  ├─ serverless.yml`
+  code3 = ` service: aws-cognito-auth-with-jwt-roles
+
+  frameworkVersion: '3'
+  
+  provider:
+    name: aws
+    runtime: python3.9
+  
+  functions:
+    hello:
+      handler: handler.hello`
+  code4 = ` ...
+  resources:
+    Resources:
+      CognitoUserPool:
+        Type: AWS::Cognito::UserPool
+        Properties:
+          UserPoolName: MyUserPool
+          UsernameAttributes:
+            - email
+          AutoVerifiedAttributes:
+            - email
+          Policies:
+            PasswordPolicy:
+              MinimumLength: 8
+              RequireLowercase: true
+              RequireNumbers: true
+              RequireSymbols: true
+              RequireUppercase: true
+              TemporaryPasswordValidityDays: 0
+          Schema:
+            - Name: email
+              Required: true
+              Mutable: false
+          EmailVerificationMessage: Hello, {####} World!
+          EmailVerificationSubject: Verify your email address`
+  code5 = ` ...
+  resources:
+    Resources:
+      CognitoUserPool:
+        ...
+      UserPoolClient:
+        Type: AWS::Cognito::UserPoolClient
+        Properties:
+          ClientName: MyUserPoolClient
+          GenerateSecret: false
+          UserPoolId: !Ref CognitoUserPool
+          AllowedOAuthFlows:
+            - code
+          AllowedOAuthFlowsUserPoolClient: true
+          AllowedOAuthScopes:
+            - email
+            - openid
+            - profile
+          CallbackURLs:
+            - https://www.google.com/
+          LogoutURLs:
+            - https://www.google.com/
+          SupportedIdentityProviders:
+            - COGNITO
+          EnableTokenRevocation: true
+          ExplicitAuthFlows:
+            - ALLOW_USER_SRP_AUTH
+            - ALLOW_REFRESH_TOKEN_AUTH
+          ReadAttributes:
+            - email
+          WriteAttributes:
+            - email`
+
+  constructor(private hljsLoader: HighlightLoader){}
+  onHighlight(e: HighlightAutoResult) {
+    this.response = {
+      language: e.language,
+      relevance: e.relevance,
+      secondBest: '{...}',
+      value: '{...}',
+    };
+  }
 }
